f0af2879e5e8c9d27a37039ebc3f117928dcc942
Flavio <flaviostincone711@gmail.com>
2026-02-26 10:51:19 +0100

    Feat: Implementazione hibernate-validator
    - Aggiunta delle annotation in InventoryDTO e in PlayerDTO

--- esercitazioni/magicForge-QuarkusReactive/src/main/java/it/exprivia/model/dto/InventoryDTO.java
+++ esercitazioni/magicForge-QuarkusReactive/src/main/java/it/exprivia/model/dto/InventoryDTO.java
@@ -1,5 +1,15 @@
 package it.exprivia.model.dto;
 
 import it.exprivia.model.enums.Item;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
-public record InventoryDTO(Item item, int quantity) {}
+public record InventoryDTO(
+    
+    @NotBlank(message = "Item cannot be null")
+    Item item, 
+    
+    @NotBlank(message = "Quantity cannot be null")
+    @Positive(message = "Quantity must be greater than 0")
+    int quantity
+) {}
